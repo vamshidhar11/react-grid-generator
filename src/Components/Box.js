935d9b3bd38f85bb13dd191306ea076dd0f24871
vamshidhar <vamshidhar.reddy@insightorb.com>
2019-11-14 20:18:33 +0530

    changed styles

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default function Box(props) {
   let style = {
     backgroundColor: '#2c666e',
-    borderRadius: '0.8rem',
     height: '10rem',
     width: '10rem',
     marginRight: '1px'
